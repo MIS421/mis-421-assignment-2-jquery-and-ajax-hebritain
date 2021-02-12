8ed81b72d3a74d1ea84e604e2d2d733f8b7e15ce
Holly Britain <hebritain@crimson.ua.edu>
2021-02-11 23:55:09 -0600

    first commit

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var len;
 var results = '';
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var fullDate = new Date();
+var timeNow;
+var fullTime;
 
 function apiSearch() {
   var params = {
@@ -10,9 +14,9 @@ function apiSearch() {
   };
 
   $.ajax({
-      url: 'my-api-url' + $.param(params),
+      url: 'https://api.bing.microsoft.com/v7.0/search?' + $.param(params),
       beforeSend: function (xhrObj) {
-        xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "my-api-key");
+          xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "47d11f659c75404bba6f65cc26456b70");
       },
       type: "GET",
     })
@@ -28,4 +32,39 @@ function apiSearch() {
     .fail(function () {
       alert("error");
     });
+}
+function getSearch(){
+    apiSearch();
+}
+function backgroundFlip() {
+    var size = numbers.length;
+    var temp = Math.floor(Math.random() * size);
+
+    document.getElementById("main").style.backgroundImage = "url(background" + temp + ".jpg)";
+}
+function getTime() {
+    hourNow = fullDate.getHours();
+    minuteNow = fullDate.getMinutes();
+    fullTime = hourNow + ":" + minuteNow;
+    console.log(fullTime);
+    document.getElementsByTagName('p').innerHTML = fullTime;
+    $('#timeHere').html(fullTime);
+    $("#time").dialog();
+    
+    /*$(function () {*/
+        /*$("#time").dialog({
+            buttons: {
+            "Okay": function () {
+            $(this).dialog("close");
+            },
+            width: 'auto',
+            height: 'auto',
+
+            open: function () {
+                
+                $("#timePlace").text(fullTime);
+            }
+            }
+        });*/
+    /*});*/
 }
